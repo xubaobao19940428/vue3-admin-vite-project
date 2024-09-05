fabf275bfad454d635429c4adb046f5823f6d514
qiancheng <915775317@qq.com>
2024-09-05 15:35:07 +0800

    增加短信验证功能

--- src/api/login.ts
+++ src/api/login.ts
@@ -3,11 +3,37 @@ interface loginParams {
     username: string
     password: string
 }
+interface sendCodeParams {
+    phonenumber: string
+}
+//获取验证码
+interface loginWithCodeParams extends sendCodeParams {
+    code: string
+}
 //登录
 export function loginAdmin(data: loginParams) {
     console.log(data)
     return request({
         url: '/login',
+        method: 'post',
+        data: data,
+    });
+}
+// 发送验证吗
+export function sendCodeFunc(data: sendCodeParams) {
+    console.log(data)
+    return request({
+        url: '/sendCode',
+        method: 'post',
+        data: data,
+    });
+}
+//短信验证吗登录
+
+export function loginWithCode(data: loginWithCodeParams) {
+    console.log(data)
+    return request({
+        url: '/loginWithCode',
         method: 'post',
         data: data,
     });
--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -29,21 +29,22 @@
                     </el-form-item>
                 </el-form>
                 <!--手机号登录-->
-                <el-form autoComplete="on" :model="loginFormData" ref="loginForm" label-position="left" label-width="0px" class="card-box" v-else>
+                <el-form autoComplete="on" :model="loginFormPhoneData" ref="loginForm" label-position="left" label-width="0px" class="card-box" v-else>
                     <el-form-item prop="username" style="border: 1px solid #ccc; padding: 0 10px; border-radius: 5px">
                         <i class="iconfont">&#xe618;</i>
-                        <el-input name="username" type="text" v-model="loginFormData.username" autoComplete="on" placeholder="账号" />
+                        <el-input name="username" type="text" v-model="loginFormPhoneData.phonenumber" autoComplete="on" placeholder="请输入手机号" />
                     </el-form-item>
-                    <el-form-item prop="password" style="border: 1px solid #ccc; padding: 0 10px; border-radius: 5px">
+                    <el-form-item prop="password" style="border: 1px solid #ccc; padding-left:10px; border-radius: 5px">
                         <i class="iconfont">&#xe658;</i>
-                        <el-input @keyup.enter.native="handleLogin" v-model="loginFormData.password" placeholder="密码" type="password"> </el-input>
+                        <el-input @keyup.enter.native="handleLogin" v-model="loginFormPhoneData.code" placeholder="请输入验证码"> </el-input>
+                        <el-button type="primary" style="height:100%" :disabled="codeTextDisabled" @click="sendCode">{{ codeText }}</el-button>
                     </el-form-item>
                     <div class="login-form-footer">
-                      <el-link type="primary">忘记密码？</el-link>
-                      <el-link type="primary">手机号登录</el-link>
+                      <el-link type="primary" :underline="false">忘记密码？</el-link>
+                      <el-link type="primary" :underline="false">手机号登录</el-link>
                     </div>
                     <el-form-item>
-                        <el-button type="primary" style="width: 100%; height: 44px" :loading="loading" @click.native.prevent="handleLogin"> 登录 </el-button>
+                        <el-button type="primary" style="width: 100%; height: 44px" :loading="loading" @click.native.prevent="handlePhoneLogin"> 登录 </el-button>
                     </el-form-item>
                 </el-form>
             </div>
@@ -55,18 +56,25 @@
 import { ref, reactive, computed } from 'vue'
 import { useUserStore } from '@/store/user'
 import { useSystemStore } from '@/store/system'
-import { loginAdmin } from '@/api/login'
+import { loginAdmin,sendCodeFunc,loginWithCode } from '@/api/login'
 import { ElMessage } from 'element-plus'
 const loading = ref(false)
 const loginType=ref('password')
+const codeText =ref('发送验证码')
+const codeTextDisabled = ref(false)
 const loginFormData = reactive({
     username: '',
     password: '',
+})
+const loginFormPhoneData = reactive({
+    phonenumber:'',
+    code:''
 })
 const userStore = useUserStore()
 const systemStore = useSystemStore()
 
 const theme = computed(() => systemStore.theme)
+
 const updateUserInfo = (userInfo) => {
     userStore.loginIn(userInfo)
 }
@@ -84,6 +92,49 @@ function handleLogin() {
         .catch((error) => {
             ElMessage.error(error.message)
         })
+}
+function sendCode(){
+    const phoneTest = /^1[3456789]\d{9}$/;
+    if(!phoneTest.test(loginFormPhoneData.phonenumber)){
+        ElMessage.error('请输入正确的手机号')
+        return
+    }
+    sendCodeFunc({phonenumber:loginFormPhoneData.phonenumber}).then(response=>{
+        if(response){
+            ElMessage.success('验证码发送成功')
+            codeTextDisabled.value=true
+            let time=60
+            const timer=setInterval(()=>{
+                time--
+                codeText.value=`${time}秒后重新发送`
+                if(time<=0){
+                    clearInterval(timer)
+                    codeText.value='发送验证码'
+                    codeTextDisabled.value=false
+                }
+            },1000)
+        }
+    })
+   
+}
+/**
+ * @description 手机号登录 
+ */
+function handlePhoneLogin(){
+    if(!loginFormPhoneData.phonenumber){
+        ElMessage.error('请输入手机号')
+        return
+    }
+    if(!loginFormPhoneData.code){
+        ElMessage.error('请输入验证码')
+        return
+    }
+    loginWithCode(loginFormPhoneData).then((response:any)=>{
+        if(response){
+            ElMessage.success('登录成功')
+            updateUserInfo(response.data)
+        }
+    })
 }
 /**
  * @description 主题切换
